cd125cc588dc7930916eb24a7535215bd99869b3
mamun700263 <mamun700263@gmail.com>
2025-02-12 17:27:38 +0600

    navbar: navbar added to all pages by templates

---
+++ js/templates.js
@@ -0,0 +1,10 @@
+// <!-- index.html -->
+
+let navbar_container = document.getElementById("navbar_container")
+
+  fetch('/html/templates/navbar.html')
+    .then(response => response.text())
+    .then(data => {
+      navbar_container.innerHTML = data;
+    });
+
